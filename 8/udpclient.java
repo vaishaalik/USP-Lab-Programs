34af991be0ef4e9bdccad688eeb6544008115a34
Vaishaali Kondapalli <vaishaalikondapalli@gmail.com>
2021-06-17 15:29:47 +0530

    Create udpclient.java

---
+++ 8/udpclient.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+public class UDPclient {
+public static void main(String args[]) throws Exception
+{
+DatagramSocket ds=new DatagramSocket(2345);
+String msg;
+
+byte[] buf=new byte[100];
+while(true)
+{
+DatagramPacket rdp=new
+DatagramPacket(buf,buf.length);
+ds.receive(rdp);
+msg=new String(rdp.getData(),rdp.getOffset(),rdp.getLength());
+System.out.println(msg);
+}
+}
+}
